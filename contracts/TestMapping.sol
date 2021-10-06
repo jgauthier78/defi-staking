e1f7737544a80c1fad742fb28eb637d661f8f25f
jgauthier78@gmail.com <jgauthier78@gmail.com>
2021-10-06 22:20:35 +0200

    Ajout d'une Struct

--- contracts/TestMapping.sol
+++ contracts/TestMapping.sol
@@ -2,27 +2,33 @@
 pragma solidity 0.8.9;
  
 contract TestMapping {
-    uint[] Cagnottes;
+    struct Cagnotte {
+        string nom;
+        uint montant;
+        uint interet;
+    }
+    Cagnotte[] Cagnottes;
     mapping(string=> uint) CagnotteMap;
     
     function AjoutCagnotte (string calldata _nom, uint _montant) public {
         int arrayIndex = int(CagnotteMap[_nom]) - 1;
         if (arrayIndex == -1) {
-            Cagnottes.push(_montant);
+            Cagnottes.push(Cagnotte(_nom, _montant, 0));
             CagnotteMap[_nom] = Cagnottes.length;
         }
         else {
-            Cagnottes[uint(arrayIndex)] = Cagnottes[uint(arrayIndex)] + _montant;
+            Cagnotte storage currentCagnotte = Cagnottes[uint(arrayIndex)];
+            currentCagnotte.montant = currentCagnotte.montant + _montant;
         }
     }
     
     function GetMontantCagnotte (string calldata _nom) public view returns (uint) {
         int arrayIndex = int(CagnotteMap[_nom]) - 1;
-        if (arrayIndex > 0) {
-            return Cagnottes[uint(arrayIndex)];
+        if (arrayIndex == -1) {
+            return 0;
         }
         else {
-            return 0;
+            return Cagnottes[uint(arrayIndex)].montant;
         }
     }
 }

ea8d1032fe781f240b2f7e5ae7e681ad397e172b
jgauthier78@gmail.com <jgauthier78@gmail.com>
2021-10-09 11:18:54 +0200

    Applying Solhint Linter rules Non fixed one = Avoid to make time-based decisions in your business logic as I did not see how to get rid of time for staking

--- contracts/AlyraStaking.sol
+++ contracts/AlyraStaking.sol
@@ -8,99 +8,102 @@ import "@OpenZeppelin/contracts/token/ERC20/IERC20.sol";
 contract AlyraStaking {
    
     // Constants
-    uint constant STAKING_RATE = 6;
+    uint private constant STAKING_RATE = 6;
     // define here what the rate refers to. Usually it would be a year = 52 weeks in solidity
     // but for test purpose it can be easier to use shorter period like a minute
-    uint constant STAKING_PERIODICITY = 1 minutes;
+    uint private constant STAKING_PERIODICITY = 1 minutes;
        
     // will store an entry information
-    // - LastReferenceDate: last date to use for calculation
-    // - StakedAmount = sum of staked amount less sum of withdrawn amounts
+    // - lastTransactionDate: last date to use for calculation
+    // - stakedAmount = sum of staked amount less sum of withdrawn amounts
     // - totalReward = reward expressed in same ERC20 token. Will be converted to "real" reward token later on, when owner wants to withdraw some
     struct Token {
-        address TokenAddress;
-        uint StakedAmount;
-        uint PreviousStakedAmountPerSecond;
-        uint LastReferenceDate;
+        address tokenAddress;
+        uint stakedAmount;
+        uint previousStakedAmountPerSecond;
+        uint lastTransactionDate;
     }
-    Token[] Tokens;
-    mapping(address => uint) TokenMap;
+    Token[] private tokens;
+    mapping(address => uint) private tokenMap;
     
-    function getNewStakedAmountPerSecond (Token memory _token) private view returns (uint){
-        return _token.PreviousStakedAmountPerSecond + ((block.timestamp - _token.LastReferenceDate) * _token.StakedAmount);
+    /// @notice calculate staked amount per second
+    /// @param token struct containing the necessary information
+    /// @return an uint
+    function getNewStakedAmountPerSecond (Token memory token) private view returns (uint){
+        return token.previousStakedAmountPerSecond + ((block.timestamp - token.lastTransactionDate) * token.stakedAmount);
     }
 
-    /// @notice calculate reward based on _token parameter
-    /// @dev simple calculation based on prorata
-    /// @param _token struct containing the necessary information (amount and LastReferenceDate)
+    /// @notice calculate reward based on token parameter
+    /// @param token struct containing the necessary information
     /// @return an uint
-    function calculateReward (Token memory _token) private view returns (uint){
-        return (getNewStakedAmountPerSecond(_token) * STAKING_RATE) / (STAKING_PERIODICITY * 100);
-        // uint elapsedTimeInSeconds = block.timestamp - token.lastReferenceDate;
-        // uint elapsedTimeInStakingPeriodicity = elapsedTimeInSeconds / STAKING_PERIODICITY;
-        // return (elapsedTimeInStakingPeriodicity * STAKING_RATE * token.totalStakedAmount) / 100;
+    function calculateReward (Token memory token) private view returns (uint){
+        return (getNewStakedAmountPerSecond(token) * STAKING_RATE) / (STAKING_PERIODICITY * 100);
     }
     
     /// @notice Stake an amount of a specific ERC20 token
-    /// @dev reward calculation is done before applying the new amount to the total
-    /// @param _tokenAddress address of the staked token
-    /// @param _amount staked amount
-    function StakeToken (address _tokenAddress, uint _amount) public {
-        require(_amount > 0, "You cannot stake 0 token");
+    /// @param tokenAddress address of the staked token
+    /// @param amount staked amount
+    function stakeToken (address tokenAddress, uint amount) public {
+        require(amount > 0, "You cannot stake 0 token");
 
-        int arrayIndex = int(TokenMap[_tokenAddress]) - 1;
+        int arrayIndex = int(tokenMap[tokenAddress]) - 1;
         if (arrayIndex == -1) {
-            Tokens.push(Token(_tokenAddress, _amount, 0, block.timestamp));
-            TokenMap[_tokenAddress] = Tokens.length;
+            tokens.push(Token(tokenAddress, amount, 0, block.timestamp));
+            tokenMap[tokenAddress] = tokens.length;
         }
         else {
-            Token storage currentToken = Tokens[uint(arrayIndex)];
-            currentToken.PreviousStakedAmountPerSecond = getNewStakedAmountPerSecond(currentToken);
-            currentToken.StakedAmount = currentToken.StakedAmount + _amount;
-            currentToken.LastReferenceDate = block.timestamp;
+            Token storage currentToken = tokens[uint(arrayIndex)];
+            currentToken.previousStakedAmountPerSecond = getNewStakedAmountPerSecond(currentToken);
+            currentToken.stakedAmount = currentToken.stakedAmount + amount;
+            currentToken.lastTransactionDate = block.timestamp;
         }
 
         // transfer amount from stakeholder to the contract
-        // stakeholder will have first approve (minimum = _amount) the contract to transfer tokens from its address
-        IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _amount);
+        // stakeholder will have first approved (minimum = amount) the contract to transfer tokens from its address
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
     }
     
     /// @notice Withdraw an amount of a specific ERC20 token
-    /// @dev reward calculation is done before withdrawing the required amount from the total
-    /// @param _tokenAddress address of the staked token
-    /// @param _amount amount to be withdrawn
-    function WithdrawToken (address _tokenAddress, uint _amount) public {
-        int arrayIndex = int(TokenMap[_tokenAddress]) - 1;
+    /// @param tokenAddress address of the staked token
+    /// @param amount amount to be withdrawn
+    function withdrawToken (address tokenAddress, uint amount) public {
+        int arrayIndex = int(tokenMap[tokenAddress]) - 1;
         require(arrayIndex > -1, "Seems you never staked the given token on this contract");
         
-        Token storage currentToken = Tokens[uint(arrayIndex)];
-        require(currentToken.StakedAmount >= _amount, "Not enough staked tokens.");
+        Token storage currentToken = tokens[uint(arrayIndex)];
+        require(currentToken.stakedAmount >= amount, "Not enough staked tokens.");
         
-        currentToken.PreviousStakedAmountPerSecond = getNewStakedAmountPerSecond(currentToken);
-        currentToken.LastReferenceDate = block.timestamp;
-        currentToken.StakedAmount = currentToken.StakedAmount - _amount;
+        currentToken.previousStakedAmountPerSecond = getNewStakedAmountPerSecond(currentToken);
+        currentToken.lastTransactionDate = block.timestamp;
+        currentToken.stakedAmount = currentToken.stakedAmount - amount;
 
         // transfer amount back to stakeholder
-        IERC20(_tokenAddress).transfer(msg.sender, _amount);
+        IERC20(tokenAddress).transfer(msg.sender, amount);
     }
     
-    function GetTokenStakedAmount (address _tokenAddress) public view returns (uint) {
-        int arrayIndex = int(TokenMap[_tokenAddress]) - 1;
+    /// @notice indicate the total staked amount of a given token 
+    /// @param tokenAddress address of the staked token
+    /// @return an uint
+    function gettokenstakedAmount (address tokenAddress) public view returns (uint) {
+        int arrayIndex = int(tokenMap[tokenAddress]) - 1;
         if (arrayIndex == -1) {
             return 0;
         }
         else {
-            return Tokens[uint(arrayIndex)].StakedAmount;
+            return tokens[uint(arrayIndex)].stakedAmount;
         }
     }
 
-    function GetTokenReward (address _tokenAddress) public view returns (uint) {
-        int arrayIndex = int(TokenMap[_tokenAddress]) - 1;
+    /// @notice indicate the calculated reward amount for a given token
+    /// @param tokenAddress address of the staked token
+    /// @return an uint
+    function getTokenReward (address tokenAddress) public view returns (uint) {
+        int arrayIndex = int(tokenMap[tokenAddress]) - 1;
         if (arrayIndex == -1) {
             return 0;
         }
         else {
-            return calculateReward(Tokens[uint(arrayIndex)]);
+            return calculateReward(tokens[uint(arrayIndex)]);
         }
     }
 }
